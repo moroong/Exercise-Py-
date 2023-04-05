24edbd876c0930c98e8b261fd5ead29d8175ec2b
moroong <122975190+moroong@users.noreply.github.com>
2023-04-05 12:26:09 +0900

    Add files via upload

--- Example01-loop.py
+++ Example01-loop.py
@@ -1,43 +1,64 @@
-# 사용자로부터 자연수 n1 을 입력 받고
-# 1 부터 n1 까지의 정수를 모두 출력하는 while문을 작성
+# 영화 평점 1 점부터 5 점 까지 출력하는 프로그램을 작성
+# 1 ~ 5 사이의 값을 입력 받고 입력한 값 만큼의 '★' 을 출력
+# 만약 범위 밖의 값을 입력을 받은 경우 다시 입력을 받도록 요구(답은 아래로)
 
+
+while True :
+    score = int(input(' 영화의 평점을 입력하세요. '))
+    if score < 1 or score > 5 :
+        print('평점은 1~5 범위 내로 입력해야합니다.')
+
+    elif score >= 1 or score <= 5 :
+        print('영화 평점 : ', end='')
+        print('★' * score)
+
+
+
+'''방법1
+
+rating = int( input( '이번 영화의 평점을 입력하세요 >>> '))
+
+# 유효한 값이 입력될 때 까지 반복
+
+while rating <= 0 or rating > 5 :
+    print(' 평점은 1 ~ 5점 사이만 입력해야 합니다. ')
+    rating = int( input( ' 이번 영화의 평점을 입력하세요>>> '))
+
+# 출력
+print('영화 평점 : ', end='')
+print("★" * rating)
 '''
-n1 = int(input('정수를 입력하세요>>>'))
 
-num = 1
+'''방법2
+# 유효한 값을 입력할 때 까지
+while True :
+    # 입력
+    rating = int( input( '이번 영화의 평점을 입력하세요 >>> '))
 
-while num <= n1 :
-    print(f'num={num}')
-    num += 1
+    # 검사
+    if rating > 0 and rating <= 5 :
+        break
+    print(' 평점은 1 ~ 5점 사이만 입력해야 합니다. ')
+
+# 출력
+print('영화 평점 : ', end='')
+print("★" * rating)
 '''
 
-# 사용자로부터 자연수 M과 N을 입력을 받고
-# M 부터 N 까지의 모든 정수를 출력하는 while 문을 작성
-#  입력 시 : N > M
+'''방법 3
+# 유효한 값을 입력할 때 까지
+while True :
+    # 입력
+    rating = int( input( '이번 영화의 평점을 입력하세요 >>> '))
 
-M, N = map(int, input('정수 2개를 입력하세요>>>').split() )
-
-num = M
-
-while num <= N :
-    print(f'num={num}')
-    num += 1
-
-# 사용자로부터 자연수 n1 을 입력을 받아 1 부터 n1 까지 출력
-# 만약 사용자가 음수를 입력한 경우 자연수 입력받도록 해야한다.
-
-n1 = int(input(' 자연수를 입력하세요 >>> '))
-
-
-while n1 < 0 :
-    print('The value entered is not positive number.')
-    n1 = int( input('자연수를 입력하세요 >>> '))
-
-# 1 부터 입력받은 n1 까지의 모든 정수를 출력
-num = 1 # 1 부터 n1 까지의 정수를 출력할 변수
-
-# num의 값이 n1이 될 때 까지 
-while num <= n1 :
-    print(f'num={num}')
-    num += 1
+    # 검사
+    if rating <= 0 and rating > 5 :
+        print(' 평점은 1 ~ 5점 사이만 입력해야 합니다. ')
+        continue
     
+    break # 반복문 종료
+
+# 출력
+print('영화 평점 : ', end='')
+print("★" * rating)
+'''
